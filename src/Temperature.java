8094bf6aca38b1d170e2efa3d9b326e7a4d996b8
hongtayvy <hongtayvy@gmail.com>
2021-11-10 13:42:58 -0600

    chore: initialized project

---
+++ src/Temperature.java
@@ -0,0 +1,19 @@
+public class Temperature {
+    public static void main(String [] args){
+        /**
+         * You will be creating a program that will convert temperatures.
+         * 1. Your program should be able to do conversion from fahrenheit to celsius
+         * 2. Your program should be able to do conversion from celsius to fahrenheit
+         *
+         * How this will work:
+         * 1. You will prompt the user for what conversion they're looking to do:
+         *  - a fahrenheit to celsius conversion
+         *  - a celsius to fahrenheit conversion
+         * 2. Once there, you will ask them what temperature they would like to convert
+         * 3. You will do the conversion
+         *  - fahrenheit to celsius equation:
+         *  - celsius to fahrenheit equation:
+         * 4. Print out the converted temperature.
+         */
+    }
+}
